65b58ca695d9df7cf687f9ed1d29706ceb799bf0
Jeddy Mweru <jeddymweru@gmail.com>
2023-04-22 10:45:41 +0000

    Two flask routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''A script that starts a Flask web application with 2 routes'''
+from flask import Flask
+
+# create an instance of this class
+app = Flask(__name__)
+
+
+# use route() to tell Flask what URL should trigger our function
+@app.route('/', strict_slashes=False)
+def hello():
+    '''Function to output Hello HBNB!'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    '''Return HBNB'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
